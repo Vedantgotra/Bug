2de4fbaec528e87ed61c288bf8596981d075f4b6
vedant gotra saas <vedantgotra.saas853@webkul.in>
2023-04-24 20:36:19 +0530

    added rotate

--- move.js
+++ move.js
@@ -1,40 +1,44 @@
 $(document).ready(function(){
     animateDiv('.cockroach');
+
 });
 
 function makeNewPosition(){
     
     var h = $(window).height() - 50;
     var w = $(window).width() - 50;
+    var rect = image.getBoundingClientRect();
+   console.log(rect.x,rect.y)
     
-
     
     var nh = Math.floor(Math.random() * h);
     var nw = Math.floor(Math.random() * w);
     
+    var p1 = {
+        x: rect.x,
+        y: rect.y
+    };
+    
+    var p2 = {
+        x: nh,
+        y: nw
+    };
+    
+    var angleDeg = Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
+    console.log(nh,nw)
+    console.log(angleDeg)
     return [nh,nw];    
     
 }
 
 function animateDiv(myclass){
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 4000,   function(){
-      animateDiv(myclass);        
-    });
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 10000,   function(){
+        rotateDiv();
+        animateDiv(myclass);        
+      });
     
 };
-var p1 = {
-	x: 20,
-	y: 20
-};
-
-var p2 = {
-	x: 40,
-	y: 40
-};
-
-// angle in radians
-var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
-
-// angle in degrees
-var angleDeg = Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
+function rotateDiv(){
+$("#image").css("transform", "rotate(20deg)");
+}
