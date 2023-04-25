1d8fc100b01dec3b6a75fab6c3f766c77150935d
Vedant Gotra <vedantgotra20@gmail.com>
2023-04-25 23:48:53 +0530

    cockroach rotation working

--- move.js
+++ move.js
@@ -1,45 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function () {
     animateDiv('.cockroach');
 
 });
 
-function makeNewPosition(){
-    
+function makeNewPosition() {
+
     var h = $(window).height() - 50;
     var w = $(window).width() - 50;
     var rect = image.getBoundingClientRect();
-   console.log(rect.x,rect.y)
-    
-    
+    console.log(rect.y, rect.x)
+
+
     var nh = Math.floor(Math.random() * h);
     var nw = Math.floor(Math.random() * w);
-    
+
     var p1 = {
-        x: rect.x,
-        y: rect.y
+        x: rect.y,
+        y: rect.x
     };
-    
+
     var p2 = {
         x: nh,
         y: nw
     };
+
+    var angleDeg = 180-Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
     
-    var angleDeg = Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
-    console.log(nh,nw)
+        // if (angleDeg >= 0 && angleDeg <= 180) {
+        //     angleDeg = 180 - angleDeg;
+        // }
+        // else if (angleDeg >= 181 && angleDeg <= 360) {
+        //     angleDeg = 360+angleDeg;
+        // }
+    
+    console.log(nh, nw)
     console.log(angleDeg)
-    return [nh,nw,angleDeg];    
-    
+    return [nh, nw, angleDeg];
+
 }
 
-function animateDiv(myclass){
+function animateDiv(myclass) {
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 10000,   function(){
-        rotateDiv(newq[2]);
-        animateDiv(myclass);        
-      });
-    
+    $('#image').animate(
+        { deg: newq[2] },
+        {
+          duration: 2000,
+          step: function(now) {
+            $(this).css({ transform: 'rotate(' + now + 'deg)' });
+          }
+        }
+      );
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 4000, function () {
+        animateDiv(myclass);
+    });
+
 };
-function rotateDiv(angle){
-    console.log(angle)
-$("#image").css("transform", "rotate("+angle+"deg)");
-}
