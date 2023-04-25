7c910f69d2cbf767944142ae7e33312c23c898b0
Vedant Gotra <vedantgotra20@gmail.com>
2023-04-25 22:10:47 +0530

    pull

--- move.js
+++ move.js
@@ -33,7 +33,7 @@ function makeNewPosition(){
 
 function animateDiv(myclass){
     var newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 2000,   function(){
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 10000,   function(){
         rotateDiv(newq[2]);
         animateDiv(myclass);        
       });
